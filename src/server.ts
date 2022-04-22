11b414c93234137857182af8ba59b7bcae0e55be
Tayse <tayse_rosa@yahoo.com.br>
2022-04-22 13:58:24 -0300

    Curso de Node JS

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,21 @@
 import express, { Request, Response } from 'express';
+import path from 'path';
+import mustache from 'mustache-express';
+import mainRoutes from './routes/index';
 
 const server = express();
 
-server.get('/', (req:Request, res:Response)=>{
+server.set('view engine', 'mustache');
+server.set('views',path.join(__dirname, 'views'));
+server.engine('mustache', mustache());
 
+/* Torna a pagina estatica */
+server.use( express.static(path.join(__dirname, '../public')));
 
-  res.send('Olá mundo!');
-})
+server.use('/',mainRoutes);
 
-server.get('/noticia/:slug', (req:Request, res:Response)=>{
-  let slug = req.params.slug;//Rota dinâmica
-  res.send(`Notícia: ${slug}`);
-});
-
-server.get('/voo/:origem-:destino', (req:Request, res:Response)=>{
-  /* let { origem, destino } = req.params;//Rota dinâmica */
-  let origem = req.params.origem;//Rota dinâmica
-  let destino = req.params.destino;//Rota dinâmica
-  res.send(`Procurando vôo de : ${origem.toUpperCase()} até ${destino.toLocaleUpperCase()}`);
+server.use((req:Request, res:Response)=>{
+  res.status(404).send('Página não encontrada!');
 });
 
 server.listen(3000);
